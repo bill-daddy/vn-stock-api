3273071c4766b08e763593e7026e4dfb7c4f2d37
tung.tran <tung.tran@asnet.com.vn>
2022-03-13 20:47:21 +0700

    chore: add example for express

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server started");
+})
